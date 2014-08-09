55c5da5e78445a71011cd89aeb580ce107913cb6
Luis Navarro <luis_navarro@mba.berkeley.edu>
2014-08-09 10:11:29 -0700

    Added upload handler

--- main.py
+++ main.py
@@ -20,6 +20,7 @@ import webapp2
 from google.appengine.api import users
 from google.appengine.ext import blobstore
 from google.appengine.ext import db
+from google.appengine.ext.webapp import blobstore_handlers
 
 template_env = jinja2.Environment( loader=jinja2.FileSystemLoader(os.getcwd()))
 
@@ -53,6 +54,19 @@ class MainPage(webapp2.RequestHandler):
         }
 
         self.response.write(template.render(context))
+
+class UploadHandler(blobstore_handlers.BlobstoreUploadHandler):
+    def post(self):
+        user = users.get_current_user()
+        description = self.request.params['description']
+
+        for blob_info in self.get_uploads('upload'):
+            upload = UserUpload( parent=db.Key.from_path('UserUploadGroup', user.email()),
+                                 user=user,
+                                 description=description,
+                                 blob=blob_info.key())
+            upload.put()
+        self.redirect('/')
 
 app = webapp2.WSGIApplication([
     ('/', MainPage)
