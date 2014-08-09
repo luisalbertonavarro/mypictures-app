3b15f4798580807a89969dac8267838b089bfc76
Luis Navarro <luis_navarro@mba.berkeley.edu>
2014-08-09 09:02:58 -0700

    Added home.html and corresponding MainPage class

--- main.py
+++ main.py
@@ -14,12 +14,46 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+import jinja2
+import os
 import webapp2
+from google.appengine.api import users
+from google.appengine.ext import blobstore
+from google.appengine.ext import db
 
-class MainHandler(webapp2.RequestHandler):
+template_env = jinja2.Environment( loader=jinja2.FileSystemLoader(os.getcwd()))
+
+class UserUpload(db.Model):
+    user = db.UserProperty()
+    description = db.StringProperty()
+    blob = blobstore.BlobReferenceProperty()
+
+class MainPage(webapp2.RequestHandler):
     def get(self):
-        self.response.write('Hello world!')
+        user = users.get_current_user()
+        login_url = users.create_login_url(self.request.path)
+        logout_url = users.create_logout_url(self.request.path)
+
+        uploads = None
+        if user:
+            q = UserUpload.all()
+            q.filter('user =', user)
+            q.ancestor(db.Key.from_path('UserUploadGroup', user.email()))
+            uploads = q.fetch(100)
+
+        upload_url = blobstore.create_upload_url('/upload')
+
+        template = template_env.get_template('home.html')
+        context = {
+            'user': user,
+            'login_url': login_url,
+            'logout_url': logout_url,
+            'uploads': uploads,
+            'upload_url': upload_url,
+        }
+
+        self.response.write(template.render(context))
 
 app = webapp2.WSGIApplication([
-    ('/', MainHandler)
+    ('/', MainPage)
 ], debug=True)
